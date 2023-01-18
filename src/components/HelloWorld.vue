b69762e44737e596d4a444ccab936956a646b138
Rodney Neville <rodneyneville@gmail.com>
2023-01-18 11:31:45 -0300

    frontend init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,40 +2,62 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      Busca de Operadoras de Saúde<br>
+
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+   
+    <p><input type="text" v-model="operadoras"></p>
+    
+    <p><button type = "button" id = "get" @click = "fetchAPIData">BUSCAR</button></p>
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+
+  data() {
+    return {
+      operadoras: '',
+    };
+  },
+
   props: {
-    msg: String
-  }
+    msg: String,
+    
+  },
+  methods: {
+        
+
+        fetchAPIData( ) { 
+
+          
+            fetch("http://127.0.0.1:8000/operadoras/"+this.operadoras, {
+                "method": "GET",
+                "headers": {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Request-Method': '*',
+                    'Accept':'*'
+                }
+            })
+            .then(response => { 
+                if(response.ok){
+                    return response.json()    
+                } else{
+                    alert("Server returned " + response.status + " : " + response.statusText);
+                }                
+            })
+            .then(response => {
+                this.result = response.body; 
+                this.responseAvailable = true;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+        }
+    }
 }
 </script>
 
